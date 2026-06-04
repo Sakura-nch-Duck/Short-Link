c583ae54627e0d4d167425ac8a65ee20dacb76fb
nch <nch1227975771@163.com>
2026-06-04 22:51:06 +0800

    nch

--- admin/src/main/java/com/nch/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nch/shortlink/admin/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.nch.shortlink.admin.controller;
 
 
+import com.nch.shortlink.admin.dto.resq.UserRespDTO;
+import com.nch.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
  * 用户管理控制层
  */
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
 
     /**
      * 根据用户名查询用户信息
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public String getUserByUsername(@PathVariable("username") String username) {
-        return "Hi," + username;
+    public UserRespDTO getUserByUsername(@PathVariable("username") String username) {
+
+        return userService.getUserByUsername(username);
     }
 }
--- admin/src/main/java/com/nch/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/nch/shortlink/admin/service/impl/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.nch.shortlink.admin.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.nch.shortlink.admin.dao.entity.UserDO;
 import com.nch.shortlink.admin.dao.mapper.UserMapper;
+import com.nch.shortlink.admin.dto.resq.UserRespDTO;
 import com.nch.shortlink.admin.service.UserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,5 +16,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
-
+    @Override
+    public UserRespDTO getUserByUsername(String username){
+        LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
+                .eq(UserDO::getUsername, username);
+        UserDO userDO = baseMapper.selectOne(queryWrapper);
+        UserRespDTO result = new UserRespDTO();
+        BeanUtils.copyProperties(userDO,result);
+        return result;
+    }
 }
